0480f06a69b97a173b9ee6ff6223638b5ff16015
Christian E <christianelena92@gmail.com>
2022-06-13 09:10:35 -0500

    install react-router-dom v5 and Material UI styled components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Header } from "./components/home/header/Header";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
--- src/components/home/header/Header.jsx
+++ src/components/home/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 export const Header = () => {
   const [sidebar, setSidebar] = useState(false);
 
